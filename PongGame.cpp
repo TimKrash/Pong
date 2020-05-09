daafab4364da1d6559379c58dd8b4d6d922809b2
TimKrash <timothy.krashevsky@gmail.com>
2020-05-09 00:11:20 -0500

    Initial build finished

--- PongGame.cpp
+++ PongGame.cpp
@@ -47,6 +47,44 @@ public:
 
 };
 
+class Score {
+private:
+    int player_score;
+    Vector2D position;
+public:
+
+    Score(int score, Vector2D position) :
+        player_score(score),
+        position(position)
+    {}
+
+    void setScore(int score) {
+        this->player_score = score;
+    }
+
+    float getScore() {
+        return player_score;
+    }
+
+    void setScorePosition(Vector2D pos) {
+        this->position = pos;
+    }
+
+    Vector2D getScorePosition() {
+        return position;
+    }
+
+    void Render(sf::RenderTarget& target) {
+        sf::Font font;
+        font.loadFromFile("Quicksand-Bold.otf");
+        sf::Text score(std::to_string(player_score), font, 20);
+        Vector2D scorePos = getScorePosition();
+        score.setPosition({ scorePos.x, scorePos.y });
+        score.setFillColor(sf::Color::White);
+        target.draw(score);
+    }
+};
+
 class Paddle {
 private:
     RectangleShape m_paddle;
@@ -58,7 +96,7 @@ public:
         m_paddle.y = starting_pos.y;
         m_paddle.width = 10;
         m_paddle.height = 80;
-        m_velocity = { 0, 2 };
+        m_velocity = { 0, 3 };
     }
 
     void setVelocity(Vector2D velocity) {
@@ -98,7 +136,7 @@ private:
 public:
     Ball() :
         pos({ WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 }),
-        vel({ 2, 0 }),
+        vel({ 5, 0 }),
         radius(5),
         target(target),
         callback([](){}),
@@ -159,8 +197,12 @@ private:
     Ball ball;
     INGAME_STATE ingame_state;
     sf::RenderTarget& m_target;
+    float play_count;
+    Score player_one_score;
+    Score player_two_score;
     bool ball_forward;
     bool ball_backward;
+    bool scoreChange;
 
 public:
 
@@ -169,16 +211,25 @@ public:
         playerOneScore(0),
         playerTwoScore(0),
         maxScore(5),
-        court({0, 0, WINDOW_WIDTH, WINDOW_HEIGHT}),
-        paddle_one({0, WINDOW_HEIGHT/2}),
-        paddle_two({WINDOW_WIDTH - 10, WINDOW_HEIGHT/2}),
+        court({ 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT }),
+        paddle_one({ 0, WINDOW_HEIGHT / 2 }),
+        paddle_two({ WINDOW_WIDTH - 10, WINDOW_HEIGHT / 2 }),
         ingame_state(PLAYING),
         ball_forward(true),
-        ball_backward(false)
+        ball_backward(false),
+        play_count(0),
+        player_one_score(0, { WINDOW_WIDTH / 4, WINDOW_HEIGHT / 4 }),
+        player_two_score(0, { 3 * WINDOW_WIDTH / 4, 3 * WINDOW_HEIGHT / 4 }),
+        scoreChange(false)
     {
         // ball.setCallback([this]() {ball_off_one = true; });
     }
 
+    /*
+   --------------
+   Handle serving state of ball
+   --------------
+   */
     SERVE_STATE handleServeState(Vector2D ball_pos, Paddle paddle_one, Paddle paddle_two) {
         if (std::floor(ball_pos.x) <= 0 || (ingame_state == PAUSED && ball_pos.x == paddle_two.getPositionSize().x - paddle_two.getPositionSize().width)) {
             return SERVE_STATE::PLAYER_TWO;
@@ -189,12 +240,17 @@ public:
         return SERVE_STATE::MIDDLE;
     }
 
+    /*
+   --------------
+   Move ball forward
+   --------------
+   */
     void ballForward(Vector2D& ball_pos, float elapsedTime, float& speed, RectangleShape& paddle_pos2) {
-        ball_pos = { ball_pos.x + ((ball.getVelocity().x * elapsedTime) / 1000), ball_pos.y - ((ball.getVelocity().y * elapsedTime) / 1000) };
+        ball_pos = { ball_pos.x + ((ball.getVelocity().x * elapsedTime) / 100), ball_pos.y - ((ball.getVelocity().y * elapsedTime) / 100) };
         ball.setPosition(ball_pos);
 
-        if (std::floor(ball_pos.x) == paddle_pos2.x && (std::floor(ball_pos.y) >= paddle_pos2.y &&
-            std::floor(ball_pos.y) <= paddle_pos2.y + paddle_pos2.height)) {
+        if (ball_pos.x >= paddle_pos2.x && (ball_pos.y >= paddle_pos2.y &&
+            ball_pos.y <= paddle_pos2.y + paddle_pos2.height) && ingame_state == PLAYING) {
             float rel_intersect_y1 = (paddle_pos2.y + paddle_pos2.height / 2) - ball_pos.y;
             float norm_intersect_y1 = rel_intersect_y1 / (paddle_pos2.height / 2);
             float bounceAngle1 = norm_intersect_y1 * MAXBOUNCEANGLE;
@@ -204,12 +260,17 @@ public:
         }
     }
 
+    /*
+   --------------
+   Move ball backward
+   --------------
+   */
     void ballBackward(Vector2D& ball_pos, float elapsedTime, float& speed, RectangleShape& paddle_pos1) {
-        ball_pos = { ball_pos.x - ((ball.getVelocity().x * elapsedTime) / 1000), ball_pos.y - ((ball.getVelocity().y * elapsedTime) / 1000) };
+        ball_pos = { ball_pos.x - ((ball.getVelocity().x * elapsedTime) / 100), ball_pos.y - ((ball.getVelocity().y * elapsedTime) / 100) };
         ball.setPosition(ball_pos);
 
-        if (std::floor(ball_pos.x) == paddle_pos1.x + paddle_pos1.width && (std::floor(ball_pos.y) >= paddle_pos1.y &&
-            std::floor(ball_pos.y) <= paddle_pos1.y + paddle_pos1.height)) {
+        if (ball_pos.x <= paddle_pos1.x + paddle_pos1.width && (ball_pos.y >= paddle_pos1.y &&
+            ball_pos.y <= paddle_pos1.y + paddle_pos1.height) && ingame_state == PLAYING) {
             float rel_intersect_y2 = (paddle_pos1.y + paddle_pos1.height / 2) - ball_pos.y;
             float norm_intersect_y2 = rel_intersect_y2 / (paddle_pos1.height / 2);
             float bounceAngle2 = norm_intersect_y2 * MAXBOUNCEANGLE;
@@ -219,11 +280,51 @@ public:
         }
     }
 
+    /*
+   --------------
+   Handle ball speed with continous passes
+   --------------
+   */
+    void handleSpeed(Vector2D& ball_pos, SERVE_STATE serve_state, float& speed) {
+        if ((std::floor(ball_pos.x) == paddle_one.getPositionSize().x + paddle_one.getPositionSize().width ||
+            std::floor(ball_pos.x) == paddle_two.getPositionSize().x - paddle_two.getPositionSize().width) &&
+            serve_state == MIDDLE) {
+            play_count++;
+            ball.setVelocity({ ball.getVelocity().x + play_count / 10, ball.getVelocity().y + play_count / 10 });
+        }
+    }
+
+    /*
+    --------------
+    Handle scoring event for players
+    --------------
+    */
+    void handleScore(SERVE_STATE serve_state) {
+        sf::Font font;
+        font.loadFromFile("Quicksand-Bold.otf");
+        int p1 = player_one_score.getScore();
+        int p2 = player_two_score.getScore();
+
+        if (serve_state == PLAYER_ONE && scoreChange == false) {
+            p1++;
+            player_one_score.setScore(p1);
+            scoreChange = true;
+        }
+        else if (serve_state == PLAYER_TWO && scoreChange == false) {
+            p2++;
+            player_two_score.setScore(p2);
+            scoreChange = true;
+        }
+    }
+
     // Insert game logic here
     GAME_STATE Update(const float elapsedTime, Vector2D mouse_pos) {
         
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
             ball.setPosition({ WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 });
+            ball.setVelocity({ 5, 0 });
+            paddle_one.setPositionSize({ 0, WINDOW_HEIGHT / 2, 10, 80});
+            paddle_two.setPositionSize({ WINDOW_WIDTH - 10, WINDOW_HEIGHT / 2, 10, 80});
             return GAME_STATE::MENU;
         }
 
@@ -235,20 +336,20 @@ public:
         RectangleShape paddle_pos1 = paddle_one.getPositionSize();
         RectangleShape paddle_pos2 = paddle_two.getPositionSize();
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && (std::floor(paddle_pos1.y) != 0)) {
-            paddle_pos1.y -= paddle_one.getVelocity().y * elapsedTime / 1000;
+            paddle_pos1.y -= paddle_one.getVelocity().y * elapsedTime / 100;
             paddle_one.setPositionSize(paddle_pos1);
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && (std::floor(paddle_pos1.y + paddle_pos1.height) != WINDOW_HEIGHT)) {
-            paddle_pos1.y += paddle_one.getVelocity().y * elapsedTime / 1000;
+            paddle_pos1.y += paddle_one.getVelocity().y * elapsedTime / 100;
             paddle_one.setPositionSize(paddle_pos1);
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && (std::floor(paddle_pos2.y) != 0)) {
-            paddle_pos2.y -= paddle_two.getVelocity().y * elapsedTime / 1000;
+            paddle_pos2.y -= paddle_two.getVelocity().y * elapsedTime / 100;
             paddle_two.setPositionSize(paddle_pos2);
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && (std::floor(paddle_pos2.y + paddle_pos2.height) != WINDOW_HEIGHT)) {
-            paddle_pos2.y += paddle_two.getVelocity().y * elapsedTime / 1000;
+            paddle_pos2.y += paddle_two.getVelocity().y * elapsedTime / 100;
             paddle_two.setPositionSize(paddle_pos2);
         }
         /*
@@ -263,6 +364,8 @@ public:
         Vector2D ball_pos = ball.getPosition();
         float speed = sqrt(pow(ball.getVelocity().x, 2) + pow(ball.getVelocity().y, 2));
         SERVE_STATE serve_state = handleServeState(ball_pos, paddle_one, paddle_two);
+        handleSpeed(ball_pos, serve_state, speed);
+        handleScore(serve_state);
 
         /*
         Restart game if the serve state is either player one or player two and one to score of respective player
@@ -272,12 +375,14 @@ public:
             ball.setPosition(ball_pos);
             ball.setVelocity({ 0,0 });
             ingame_state = PAUSED;
+            play_count = 0;
         }
         else if (serve_state == SERVE_STATE::PLAYER_TWO) {
             ball_pos = { paddle_two.getPositionSize().x - paddle_two.getPositionSize().width, paddle_two.getPositionSize().y + paddle_two.getPositionSize().height / 2 };
             ball.setPosition(ball_pos);
             ball.setVelocity({ 0, 0 });
             ingame_state = PAUSED;
+            play_count = 0;
         }
 
         if (ball_forward && serve_state == SERVE_STATE::MIDDLE) {
@@ -290,20 +395,24 @@ public:
         Handle new serve start
         */
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && serve_state == PLAYER_ONE) {
-            ball.setVelocity({ 2, 0 });
+            ball.setVelocity({ 5, 0 });
             serve_state = SERVE_STATE::MIDDLE;
+            ingame_state = PLAYING;
+            scoreChange = false;
             ballForward(ball_pos, elapsedTime, speed, paddle_pos2);
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && serve_state == PLAYER_TWO) {
-            ball.setVelocity({ 2, 0 });
+            ball.setVelocity({ 5, 0 });
             serve_state = SERVE_STATE::MIDDLE;
+            ingame_state = PLAYING;
+            scoreChange = false;
             ballBackward(ball_pos, elapsedTime, speed, paddle_pos1);
         }
 
         /*
         Handle wall collisions
         */
-        if (std::floor(ball_pos.y) == court.getDimensions().y + 4 || std::floor(ball_pos.y) == WINDOW_HEIGHT - 8) {
+        if (ball_pos.y <= court.getDimensions().y + 4 || ball_pos.y >= WINDOW_HEIGHT - 8) {
             float ball_y = ball.getVelocity().y;
             ball_y *= -1;
             ball.setVelocity({ ball.getVelocity().x,  ball_y });
@@ -311,12 +420,7 @@ public:
         /*
         End handle wall conditions
         */
-
-        /*
-        Handle serving states
-        */
         
-
         return GAME_STATE::IN_GAME;
 
     }
@@ -326,5 +430,7 @@ public:
         paddle_one.Render(m_target);
         paddle_two.Render(m_target);
         ball.Render(m_target);
+        player_one_score.Render(m_target);
+        player_two_score.Render(m_target);
     }
 };
